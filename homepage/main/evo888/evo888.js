c41fdfbbadc6b653d0c3a2e0a39cf1ed53d76326
searchfile <5g88.offical@gmail.com>
2026-03-26 13:48:58 +0800

    Update evo888.js

--- homepage/main/evo888/evo888.js
+++ homepage/main/evo888/evo888.js
@@ -120,8 +120,8 @@ function initCustomSelect(selectId, withSearch = false, searchPlaceholder = "Sea
   const arrow = document.createElement("span");
   arrow.className = "cs-arrow";
   arrow.innerHTML = `
-  <svg viewBox="0 0 24 24" aria-hidden="true">
-    <path d="M23.245 4l-11.245 14.374-11.219-14.374-.781.619 12 15.381 12-15.391-.755-.609z"></path>
+  <svg viewBox="64 64 896 896" aria-hidden="true">
+    <path d="M884 256h-75c-5.1 0-9.9 2.5-12.9 6.6L512 654.2 227.9 262.6c-3-4.1-7.8-6.6-12.9-6.6h-75c-6.5 0-10.3 7.4-6.5 12.7l352.6 486.1c12.8 17.6 39 17.6 51.7 0l352.6-486.1c3.9-5.3.1-12.7-6.4-12.7z"></path>
   </svg>
   `;
 
